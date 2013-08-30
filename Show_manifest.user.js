4e41ff2589f1ee9ed7fce57d447a250d8fc6bf4a
David Plassmann <dplassmann@ngpvan.com>
2013-08-30 12:22:31 -0400

    Start getting and parsing manifest

---
+++ Show_manifest.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        Show manifest
+// @namespace   NGPVAN
+// @description Display the version manifest on the login screen
+// @include     http*://localhost/Security/SelectAccount*
+// @include     http*://*.oberon.local/Security/SelectAccount*
+// @include     http*://*.myngp.com/Security/SelectAccount*
+// @include		http*://oberon.ngpsoftware.com/Security/SelectAccount*
+// @version     1.0
+// @grant    GM_addStyle
+// ==/UserScript==
+ 
+GM_xmlhttpRequest({
+	method: 'GET',
+	dataType: 'jsonp',
+	url: 'http://localhost:82/Api/v2/Manifest',
+	onload: function (response) {
+		var data = JSON.parse(response.responseText);
+		var newHTML = document.createElement('div'); 
+		newHTML.innerHTML='<div id="manifest">'
+			+'Version: '+data.version
+			+'</div>'; 
+		document.body.appendChild(newHTML);
+	}, 
+});
